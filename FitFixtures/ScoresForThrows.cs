fcef4424eb860aa23dc142af24a5e9f0b276d415
Shane Currier <shane@enlearn.com>
2015-03-14 07:58:49 -0700

    Initial commit with working single test in fitness

---
+++ FitFixtures/ScoresForThrows.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class ScoresForThrows
+    {
+        public void setScoreAs(string name)
+        {
+
+        }
+
+        public void setRoll(int[] roll)
+        {
+
+        }
+
+        public int score()
+        {
+            return 0;
+        }
+    }
