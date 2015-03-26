00cdb2e60cdcf4043b59fb3d0d625789fea10859
Shane Currier <shane@enlearn.com>
2015-03-26 15:49:21 -0700

    Ones scores for zero

---
+++ Nerdzee/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nerdzee
+{
+    public class Game
+    {
+        public int Score(Rule rule, int[] p)
+        {
+            return 0;
+        }
+    }
+}
---
+++ NunitTests/SampleTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Nerdzee;
+
+namespace NunitTests
+{
+    /// <summary>
+    /// A test class for ...
+    /// </summary>
+    [TestFixture]
+    public class NerdzeeTest
+    {
+
+        [Test]
+        public void Ones()
+        {
+            var testObj = new Nerdzee.Game();
+            Assert.AreEqual(0, testObj.Score(Rule.Ones, new []{2,3,4,5,6}));
+        }
+    }
+}
