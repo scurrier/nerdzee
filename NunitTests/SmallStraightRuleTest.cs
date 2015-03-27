dd4e53e73c64b0c03c3692cf178ebabc1066e3af
Shane Currier <shane@enlearn.com>
2015-03-27 11:12:38 -0700

    Refactor small straight test to add setup

--- NunitTests/SmallStraightRuleTest.cs
+++ NunitTests/SmallStraightRuleTest.cs
@@ -10,52 +10,52 @@ namespace NunitTests
     [TestFixture]
     public class SmalltraightRuleTest
     {
+        private SmallStraightRule testObj;
+        [SetUp]
+        public void SetUp()
+        {
+            testObj = new SmallStraightRule();
+        }
+
         [Test]
         public void ScoreSmallStraight()
         {
-            var testObj = new SmallStraightRule();
             Assert.AreEqual(30, testObj.Score(new[] { 2, 3, 4, 5, 6 }));
         }
 
         [Test]
         public void ScoreUnsortedSmallStraight()
         {
-            var testObj = new SmallStraightRule();
             Assert.AreEqual(30, testObj.Score(new[] { 2, 4, 3, 5, 6 }));
         }
 
         [Test]
         public void ScoreInvalidSmallStraight()
         {
-            var testObj = new SmallStraightRule();
             Assert.AreEqual(0, testObj.Score(new[] { 1, 2, 4, 5, 6 }));
         }
 
         [Test]
         public void ScoreActuallySmallStraight()
         {
-            var testObj = new SmallStraightRule();
             Assert.AreEqual(30, testObj.Score(new[] { 1, 3, 4, 5, 6 }));
         }
 
         [Test]
         public void ScoreSmallStraightAtBeginning()
         {
-            var testObj = new SmallStraightRule();
             Assert.AreEqual(30, testObj.Score(new[] { 1, 3, 4, 2, 7 }));
         }
 
         [Test]
         public void ScoreStraightOfLengthThree()
         {
-            var testObj = new SmallStraightRule();
             Assert.AreEqual(0, testObj.Score(new[] { 1, 3, 5, 2, 7 }));
         }
 
         [Test]
         public void ScoreSmallStraightWithDuplicates()
         {
-            var testObj = new SmallStraightRule();
             Assert.AreEqual(30, testObj.Score(new[] { 1, 3, 3, 2, 4 }));
         }
     }
