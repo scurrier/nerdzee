adc2ce50e0f7e53fa5bded85f0f27c5367b21603
Shane Currier <shane@enlearn.com>
2015-03-27 10:29:52 -0700

    Large straight.

---
+++ Nerdzee/LargeStraightRule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nerdzee
+{
+    public class LargeStraightRule
+    {
+        public LargeStraightRule()
+        {
+        }
+
+        public int Score(int[] rolls)
+        {
+            Array.Sort(rolls);
+            for (int index = 0; index < rolls.Length - 1; index++ )
+            {
+                if (rolls[index] + 1 != rolls[index + 1])
+                    return 0;
+            }
+            return 30;
+        }
+    }
+}
---
+++ NunitTests/LargeStraightRuleTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Nerdzee;
+
+namespace NunitTests
+{
+    [TestFixture]
+    public class LargeStraightRuleTest
+    {
+        [Test]
+        public void ScoreLargeStraight()
+        {
+            var testObj = new LargeStraightRule();
+            Assert.AreEqual(30, testObj.Score(new[] { 2, 3, 4, 5, 6 }));
+        }
+
+        [Test]
+        public void ScoreUnsortedLargeStraight()
+        {
+            var testObj = new LargeStraightRule();
+            Assert.AreEqual(30, testObj.Score(new[] { 2, 4, 3, 5, 6 }));
+        }
+
+        [Test]
+        public void ScoreInvalidLargeStraight()
+        {
+            var testObj = new LargeStraightRule();
+            Assert.AreEqual(0, testObj.Score(new[] { 1, 2, 4, 5, 6 }));
+        }
+
+        [Test]
+        public void Create()
+        {
+        }
+    }
+}
