ef404bbe7bd63a4c2d581d04d743841c3e53be2d
Shane Currier <shane@enlearn.com>
2015-03-27 11:49:26 -0700

    hook up the Fit tests for suggestions

---
+++ FitFixtures/SuggestedScores.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class SuggestedScores
+{
+    private int[] roll;
+
+    public void setroll(int[] roll)
+    {
+        this.roll = roll;
+    }
+
+    public string[] suggestion()
+    {
+        return new List<string>() { "AllOfAKind" }.ToArray();
+    }
+}
+
